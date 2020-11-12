42863cd87d61b6aca5459f812dc519f3aec670a0
mishraadarsh602 <66960551+mishraadarsh602@users.noreply.github.com>
2020-11-12 16:09:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 // When the user scrolls down 50px from the top of the document, resize the header's font size
 window.onscroll = function() {scrollFunction()};   
 function scrollFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+  if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
     document.getElementById("fixx2").style.width = "170px";
     document.getElementById("fixx2").style.height = "60px";
   } else {
