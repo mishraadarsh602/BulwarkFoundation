0e701e75a7a67970628dcc0dc03684703e37de6b
mishraadarsh602 <66960551+mishraadarsh602@users.noreply.github.com>
2020-11-15 18:25:34 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,14 +3,23 @@
 window.onscroll = function() {scrollFunction()};   
 function scrollFunction() {
   if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
-    document.getElementById("fixx2").style.width = "170px";
+  document.getElementById("fixx2").style.width = "170px";
     document.getElementById("fixx2").style.height = "60px";
   } else {
-    document.getElementById("fixx2").style.width = "1000px";
-    document.getElementById("fixx2").style.height = "300px";
+   document.getElementById("fixx2").style.width = "70%";
+    document.getElementById("fixx2").style.height = "40%";
   }
 
  
+  if (document.body.scrollTop >780 || document.documentElement.scrollTop >780) {
+    document.getElementById("script-logo").style.display = "none";
+      document.getElementById("script-logo1").style.display = "none";
+     
+    } else {
+     document.getElementById("script-logo").style.display = "block";
+      document.getElementById("script-logo1").style.display = "block";
+    }
+}
 
 
 
